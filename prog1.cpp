3ec14f7bf731b481665ec2a389c35c8d09ab1c36
Gama Aristondo <gamaliel@cs.ucsb.edu>
2022-04-14 15:23:02 -0700

    changed ints to floats, need to stop repeat values

--- prog1.cpp
+++ prog1.cpp
@@ -9,15 +9,15 @@
 using namespace std;
 
 struct Point {
-    int x,y;
+    float x,y;
 };
 
-int lineDst(Point p1, Point p2, Point q) {
+float lineDst(Point p1, Point p2, Point q) {
     return abs( (q.y - p1.y) * (p2.x - p1.x) - (p2.y - p1.y) * (q.x - p1.x) );
 }
 
-int side(Point p1, Point p2, Point q) {
-    int s = (q.y - p1.y) * (p2.x - p1.x) - (p2.y - p1.y) * (q.x - p1.x);
+float side(Point p1, Point p2, Point q) {
+    float s = (q.y - p1.y) * (p2.x - p1.x) - (p2.y - p1.y) * (q.x - p1.x);
 
     if(s > 0)
         return 1;
@@ -31,11 +31,11 @@ void customSort(vector<Point>& v);
 vector<Point> FindHull(vector<Point> p, Point min, Point max, int s, int n){
     vector<Point> left, right, res;
 
-    int index = -1, maxDst = 0;
+    float index = -1, maxDst = 0;
 
     for (int i = 0; i < n; i++) {
-        int dist = lineDst(min, max, p[i]);
-        int pointSide = side(min,max,p[i]);
+        float dist = lineDst(min, max, p[i]);
+        float pointSide = side(min,max,p[i]);
         if( (pointSide == s) && (dist > maxDst) ) {
             index = i;
             maxDst = dist;
@@ -52,7 +52,6 @@ vector<Point> FindHull(vector<Point> p, Point min, Point max, int s, int n){
     // res.reserve( left.size() + right.size() );
     res.insert( res.end(), left.begin(), left.end() );
     res.insert( res.end(), right.begin(), right.end() );
-    return res;
 
     return res;
 }
@@ -79,7 +78,8 @@ vector<Point> QuickHull(vector<Point> p, int n) {
 }
 
 int main() {
-    int n, i, x, y;
+    int n, i;
+    float x, y;
     i = 0;
     cin >> n;
     string input, line;
@@ -103,10 +103,10 @@ int main() {
         ++i;
     }
     points.erase(points.begin());
-    for (int i = 0; i < points.size(); i++)
+    /*for (int i = 0; i < points.size(); i++)
     {
         cout << points[i].x << " " << points[i].y << '\n';
-    }
+    }*/
     
 
     hull = QuickHull(points, n);
