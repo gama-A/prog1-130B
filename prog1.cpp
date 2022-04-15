1329976e9d49bea52780e0f4ce1238289ce9d7db
Gama Aristondo <gamaliel@cs.ucsb.edu>
2022-04-14 20:30:44 -0700

    stopped repeat values almost passing 10 inputs

--- prog1.cpp
+++ prog1.cpp
@@ -4,20 +4,21 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <set>
 #include <vector>
 
 using namespace std;
 
-struct Point {
-    float x,y;
-};
+#define Point pair<float,float>
+
+set<Point> hull;
 
 float lineDst(Point p1, Point p2, Point q) {
-    return abs( (q.y - p1.y) * (p2.x - p1.x) - (p2.y - p1.y) * (q.x - p1.x) );
+    return abs( (q.second - p1.second) * (p2.first - p1.first) - (p2.second - p1.second) * (q.first - p1.first) );
 }
 
 float side(Point p1, Point p2, Point q) {
-    float s = (q.y - p1.y) * (p2.x - p1.x) - (p2.y - p1.y) * (q.x - p1.x);
+    float s = (q.second - p1.second) * (p2.first - p1.first) - (p2.second - p1.second) * (q.first - p1.first);
 
     if(s > 0)
         return 1;
@@ -28,9 +29,7 @@ float side(Point p1, Point p2, Point q) {
 
 void customSort(vector<Point>& v);
 
-vector<Point> FindHull(vector<Point> p, Point min, Point max, int s, int n){
-    vector<Point> left, right, res;
-
+void QuickHull(vector<Point> p, Point min, Point max, int s, int n){
     float index = -1, maxDst = 0;
 
     for (int i = 0; i < n; i++) {
@@ -43,19 +42,17 @@ vector<Point> FindHull(vector<Point> p, Point min, Point max, int s, int n){
     }
 
     if(index == -1) {
-        res.push_back(min);
-        res.push_back(max);
-        return res;
+        hull.insert(min);
+        hull.insert(max);
+        return;
     }
-    left = FindHull(p, p[index], min, -side(p[index], min, max), n);
-    right = FindHull(p, p[index], max, -side(p[index], max, min), n);
-    // res.reserve( left.size() + right.size() );
-    res.insert( res.end(), left.begin(), left.end() );
-    res.insert( res.end(), right.begin(), right.end() );
-
-    return res;
+    QuickHull(p, p[index], min, -side(p[index], min, max), n);
+    QuickHull(p, p[index], max, -side(p[index], max, min), n);
+    
+    // return res;
 }
 
+/*
 vector<Point> QuickHull(vector<Point> p, int n) {
     vector<Point> left, right, res;
 
@@ -75,7 +72,7 @@ vector<Point> QuickHull(vector<Point> p, int n) {
     res.insert( res.end(), right.begin(), right.end() );
 
     return res;
-}
+} */
 
 int main() {
     int n, i;
@@ -83,7 +80,7 @@ int main() {
     i = 0;
     cin >> n;
     string input, line;
-    vector<Point> points, hull;
+    vector<Point> points, left, right;
     while(getline(cin, line)) {
         Point temp;
 
@@ -93,8 +90,8 @@ int main() {
         getline(ss, input);
         istringstream(input) >> y;
 
-        temp.x = x;
-        temp.y = y;
+        temp.first = x;
+        temp.second = y;
 
         points.push_back(temp);
         
@@ -103,17 +100,23 @@ int main() {
         ++i;
     }
     points.erase(points.begin());
-    /*for (int i = 0; i < points.size(); i++)
-    {
-        cout << points[i].x << " " << points[i].y << '\n';
-    }*/
-    
 
-    hull = QuickHull(points, n);
+    int min_y = 0, max_y = 0;
+    for(int i = 0; i < n; i++) {
+        if(points[i].second < points[min_y].second)
+            min_y = i;
+        if (points[i].second > points[max_y].second)
+            max_y = i;
+    }
+
+    QuickHull(points, points[min_y], points[max_y], -1, n);
+    QuickHull(points, points[min_y], points[max_y], -1, n);
 
     cout << hull.size() << endl;
-    for(int i = 0; i < hull.size(); i++) {
-        cout << i << "," << hull[i].x << "," << hull[i].y << endl;
+    int j = 0;
+    for(auto i : hull) {
+        cout << j << "," << i.first << "," << i.second << '\n';
+        ++j;
     }
 
     return 0;
